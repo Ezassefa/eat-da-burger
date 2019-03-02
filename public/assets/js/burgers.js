49c9119400ec8465ac2cd78e9478d5b7683e0ebe
Ezassefa <44813292+Ezassefa@users.noreply.github.com>
2019-03-02 16:23:33 -0500

    Add files via upload

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,1 +1,43 @@
-
+$(function() {
+    // update burger status
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newdevoured");
+  
+      var newDevouredState = {
+        devoured: newDevoured
+      };
+  
+      // PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevoured);
+          location.reload();
+        }
+      );
+    });
+  
+    // New Burger
+    $(".add-burger").on("submit", function(event) {
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#burger-name").val().trim(),
+        devoured: false
+      };
+  
+      // POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          location.reload();
+        }
+      );
+    });
+  });
